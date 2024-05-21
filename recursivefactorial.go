a8b03b3a2d4980f1f98086a069a3717f1e70df4e
afethi <fethiabderrahmane1@gmail.com>
2024-05-21 15:29:25 +0100

    My pool at Zone01 OUJDA

---
+++ recursivefactorial.go
@@ -0,0 +1,14 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb <= 0 || nb > 20 {
+		if nb == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	var result int
+	result = nb * RecursiveFactorial(nb - 1)
+	return result
+}
