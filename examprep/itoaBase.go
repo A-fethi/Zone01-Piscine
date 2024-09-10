fb7c3d52639e31df91c16d7895d0a1883b647e00
Abderrahmane Fethi <fethiabderrahmane1@gmail.com>
2024-09-10 18:11:25 +0100

    itoaBase

---
+++ examprep/itoaBase.go
@@ -0,0 +1,28 @@
+package main
+
+func ItoaBase(value, base int) string {
+	if base < 2 || base > 16 {
+		return ""
+	}
+
+	digits := "0123456789ABCDEF"
+	result := ""
+	isNeg := value < 0
+	if isNeg {
+		value = -value
+	}
+
+	for value > 0 {
+		rem := value % base
+		result = string(digits[rem]) + result
+		value = value / base
+	}
+	if result == "" {
+		result = "0"
+	}
+
+	if isNeg {
+		result = "-" + result
+	}
+	return result
+}
